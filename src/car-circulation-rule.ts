fdaa2a0a817da716d6c272ab1555668440bb2d17
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2021-07-24 22:13:32 -0500

    refactor: convert days type to enum

--- src/car-circulation-rule.ts
+++ src/car-circulation-rule.ts
@@ -1,12 +1,4 @@
-type weekDay = {
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday',
-    7: 'Sunday'
-}
+import weekDay from "./resources/days";
 
 type hourRestriction = {
     initialHour: Date,
